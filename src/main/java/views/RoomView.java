1f6abb1f10b9a71168a8345e6edb97b19641e26a
polserrano8 <polserranof@gmail.com>
2025-11-03 19:00:11 +0100

    feat/updating DaoPlayer and DaoDecoration with the idroom atribute

--- src/main/java/views/RoomView.java
+++ src/main/java/views/RoomView.java
@@ -10,7 +10,6 @@ public class RoomView {
     private RoomService service = new RoomService();
     public void showRooms() throws Exception{
         System.out.println("<======== Rooms ========>");
-        System.out.println(service.getRooms().toString())
 
     }
 
